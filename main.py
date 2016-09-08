ef608ce3615b7a55c48fcbe5b0ff1b7e6b9c8c41
Jim Baker <jam.baker1217@gmail.com>
2016-09-08 16:14:09 -0500

    initial commit of build-a-blog

--- main.py
+++ main.py
@@ -1,25 +1,55 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import os
 import webapp2
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class Post(db.Model):
+
+    title = db.StringProperty(required = True)
+    new = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class MainPage(Handler):
+
+    def render_front(self, title="", new="", error=""):
+        newposts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC LIMIT 5")
+
+        self.render("base.html", title=title, new=new, error=error, newposts=newposts)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("title")
+        new = self.request.get("new")
+
+        if title and new:
+            a = Post(title = title, new = new)
+            a.put()
+
+            self.redirect("/")
+
+        else:
+            error = "We need both a title and blog post!"
+            self.render_front(title, new, error)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage)
 ], debug=True)
